4dd9463aaf07c745534dac1778cbb9e4a3b16cdf
Ananto30 <Ananto30@github.com>
2019-10-25 23:03:54 +0600

    change turn server

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,3 @@
-
 // Generate random room name if needed
 if (!location.hash) {
     location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
@@ -12,17 +11,15 @@ const roomName = 'observable-' + roomHash;
 const configuration = {
     'iceServers': [
         {
-            'urls': 'stun:stun.l.google.com:19302'
+            'urls': [
+                'stun:stun2.l.google.com:19302',
+                'stun:stun.l.google.com:19302?transport=udp',
+            ]
         },
         {
-            'urls': 'turn:192.158.29.39:3478?transport=udp',
-            'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            'username': '28224511:1379330808'
-        },
-        {
-            'urls': 'turn:192.158.29.39:3478?transport=tcp',
-            'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            'username': '28224511:1379330808'
+            urls: 'turn:numb.viagenie.ca',
+            credential: 'muazkh',
+            username: 'webrtc@live.com'
         }
     ]
 };
@@ -53,8 +50,6 @@ socket.on('connect', function () {
         startWebRTC(isOfferer);
     });
 });
-
-
 
 
 function onSuccess() {
